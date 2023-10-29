73ac24d8d959f2e8035767ef6385dfebdfc8932b
sreasgop <sreasgop@gmail.com>
2023-10-30 03:55:08 +0530

    .\03_automorphic.py spacing fixed

--- Mathematical Operations/03_automorphic.py
+++ Mathematical Operations/03_automorphic.py
@@ -1,2 +1,3 @@
 # One-liner Python program to tell whether entered numbemr is Automorphic or not.
+
 print("Automorphic Number" if (len(list((x for x in [int(input("Enter a number: "))] if str(x*x).endswith(str(x)))))==1) else "Not Automorphic Number" )
